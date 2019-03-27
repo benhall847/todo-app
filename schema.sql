3f55289094f47bbde75f8c9bd83c24da0165d41e
Benjamin Hallquist <benhall84707@gmail.com>
2019-03-27 19:35:24 -0400

    added skeleton

---
+++ schema.sql
@@ -0,0 +1,5 @@
+create table toDo (
+    id serial primary key,
+    text_content text,
+    status varchar(10)
+)
---
+++ seed.sql
@@ -0,0 +1,5 @@
+insert into toDo
+(text_content, status)
+values
+("GET MORE POOP", "pending")
+("clean the kitty litter", "done")
